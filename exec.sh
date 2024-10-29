a0c768527b80b0101adc842d3758f2ff79d8e989
alexpetul <alekspetul@gmail.com>
2024-10-29 13:50:29 +0300

    Remove .idea

--- exec.sh
+++
@@ -1,3 +0,0 @@
-zip -r Archive.zip .
-sshpass -p "5114115dom5" scp ./Archive.zip alexpetul@192.168.1.10:/home/alexpetul
-sshpass -p "5114115dom5" ssh alexpetul@192.168.1.10 -t 'unzip -o Archive.zip -d lua-project; cd lua-project; lua main.lua'
